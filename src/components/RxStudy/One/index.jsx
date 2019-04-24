44cdd8a1030d8e0e99c480bc634ee89590ac9257
wenyuanzhuo <775416111@qq.com>
2019-04-24 11:03:01 +0800

    [done] styled-component done

--- src/components/RxStudy/One/index.jsx
+++ src/components/RxStudy/One/index.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { jsx, css } from '@emotion/core';
-import styled from '@emotion/styled';
 import SC from 'common/StyledComponent/index'
 const Button = SC.Button
 const Button1 = SC.Button1
